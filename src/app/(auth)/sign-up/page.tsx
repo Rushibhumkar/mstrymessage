8ab1b395f8b5c098802a41dce92f12a8c63e192e
Abcom <abcom@Abcoms-MacBook-Pro.local>
2024-09-19 11:47:11 +0530

    first commit

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -103,10 +103,10 @@ export default function SignUpForm() {
     <div className="flex justify-center items-center min-h-screen bg-gray-800">
       <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
         <div className="text-center">
-          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+          <h1 className="text-4xl text-gray-800 font-extrabold tracking-tight lg:text-5xl mb-6">
             Join True Feedback
           </h1>
-          <p className="mb-4">Sign up to start your anonymous adventure</p>
+          <p className="mb-4 text-gray-800">Sign up to start your anonymous adventure</p>
         </div>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
@@ -115,7 +115,7 @@ export default function SignUpForm() {
               control={form.control}
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Username</FormLabel>
+                  <FormLabel className="text-gray-800">Username</FormLabel>
                   <Input
                     {...field}
                     onChange={(e) => {
@@ -123,7 +123,7 @@ export default function SignUpForm() {
                       setUsername(e.target.value);
                     }}
                   />
-                  {isCheckingUsername && <Loader2 className="animate-spin" />}
+                  {isCheckingUsername && <Loader2 className="animate-spin text-" />}
                   {!isCheckingUsername && usernameMessage && (
                     <p
                       className={`text-sm ${
@@ -172,8 +172,6 @@ export default function SignUpForm() {
                     >
                       {showPassword ? <EyeOff /> : <Eye />}
                     </span>
-                    <span></span>
-                    <p className="text-muted text-gray-400 text-sm"></p>
                   </div>
                   <FormMessage />
                 </FormItem>
